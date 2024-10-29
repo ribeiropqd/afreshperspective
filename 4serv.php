c0a9cedba150438b337bea2be8d0a919c0725240
Victor Ribeiro <163345846+ribeiropqd@users.noreply.github.com>
2024-10-29 19:58:39 -0300

    Add files via upload
    Att

---
+++ 4serv.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastre-se</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="stylecss/1homecss">
+    <link rel="stylesheet" href="stylecss/servicos.css">
+    <title>Serviços</title>
+</head>
+<body style="background-image: url(imagens/fundopag2.jpg);">
+
+<nav class="navbar navbar-expand-lg bg-body-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand text-light" href="#">AFS</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active text-light" aria-current="page" aria-current="page" href="1home.php">
+            <button class="btn btn-outline-light">HOME</button></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active text-light" href="2qs.php">
+            <button class="btn btn-outline-light">QUEM SOMOS</button></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active text-light" href="#">
+            <button class="btn btn-outline-light">CADASTRE-SE</button></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active text-light" href="#">
+            <button class="btn btn-outline-light">SERVIÇOS</button></a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-3" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
+        <button class="btn btn-outline-light" type="submit">Pesquisar</button>
+      </form>
+    </div>
+  </div>
+</nav>
+<!--SALVANDO-->
+<div class="d-flex flex-row mb-3" id="casamento">
+    <div class="p-2">
+        <div class="moldura"><!--CONTAINER PARA MOLDURA-->
+            <a href="4-servicos.html">
+            <img src="imagens/casamento.jpg" alt="fotocasamento">
+            <figcaption>FOTOGRAFIAS DE CASAMENTOS.</figcaption>
+            </a>
+        </div>
+        <div class="texto p-2">
+            <p>BEM-VINDO(A), AO NOSSO SITE DE FOTOGRAFIAS. AQUI</p>
+        </div>
+    </div>
+</div>
+    <div class="d-flex flex-row-reverse" id="formatura">
+        <div class="p-2">
+            <div class="moldura">
+                <a href="4-servicos.html">
+                <img src="imagens/casamento.jpg" alt="fotoformatura">
+                <figcaption>FOTOGRAFIAS DE FORMATURAS</figcaption>
+                </a>
+            </div>
+        </div>
+    </div>
+    
+    <!--JAVA SCRIPT-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+</body>
+</html>
